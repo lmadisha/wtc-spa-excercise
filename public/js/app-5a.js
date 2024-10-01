00aac4d5b97ac48d0ac9e736f7298c7bc850e7e7
Legae Madisha <lmadisha023@student.wethinkcode.co.za>
2024-10-01 15:42:32 +0200

    Finished the Other user interactions excercise.

--- public/js/app-5a.js
+++ public/js/app-5a.js
@@ -33,6 +33,36 @@ const routes = [
     handler();
   };
 
+  function lookupWord() {
+    const form = document.getElementById("lookup-form");
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const data = new FormData(event.target);
+        const word = data.get("word");
+
+        const options = {
+            method: 'GET',
+        };
+
+        document.getElementById('results').innerHTML = `<p>Searching for <em>${word}'</em>...</p>`;
+
+        fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`, options)
+            .then(response => response.json())
+            .then(data => {
+                data = {
+                    word: data[0].word,
+                    phonetic: data[0].phonetic,
+                    partOfSpeech: data[0].meanings[0].partOfSpeech,
+                    definitions: data[0].meanings[0].definitions
+                };
+                const template = document.getElementById('template').innerText;
+                const compiledFunction = Handlebars.compile(template);
+                document.getElementById('results').innerHTML = compiledFunction(data);
+            });
+    });;
+}
+
 
 window.addEventListener('load', router);
 window.addEventListener('hashchange', router);
